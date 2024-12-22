5c6670f94dfc19b190b346534eac57a3180c291d
SangHyun Jung <wjdtkdgus876@naver.com>
2024-12-22 20:32:30 +0900

    dto글자 수정

--- src/main/java/com/kuit/kuit4serverauth/dto/request/LoginRequest.java
+++ src/main/java/com/kuit/kuit4serverauth/dto/request/LoginRequest.java
@@ -2,9 +2,11 @@ package com.kuit.kuit4serverauth.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class LoginRequest {
     String username;
     String password;
